ba1b74dd2551ca5086217630449eaf86b35b2b1e
alex <amanrique86@gmail.com>
2026-01-20 12:19:54 +0100

    fix fmt

--- src/SecureSignatureContract.sol
+++ src/SecureSignatureContract.sol
@@ -9,93 +9,85 @@ pragma solidity ^0.8.30;
 contract SecureSignatureContract {
     mapping(address => bool) public authorizedUsers;
     mapping(bytes32 => bool) public usedHashes;
-    
+
     event UserAuthorized(address indexed user, bytes32 indexed hash);
     event InvalidSignatureRejected(address indexed signer, bytes32 indexed hash);
-    
+
     /**
      * @dev Secure function that recovers signer from signature with proper validation
      * @param v The v component of the signature
-     * @param r The r component of the signature  
+     * @param r The r component of the signature
      * @param s The s component of the signature
      * @param hash The hash that was signed
      * @param user The user to authorize
      */
-    function authorizeUser(
-        uint8 v, 
-        bytes32 r, 
-        bytes32 s, 
-        bytes32 hash,
-        address user
-    ) external {
+    function authorizeUser(uint8 v, bytes32 r, bytes32 s, bytes32 hash, address user) external {
         // SECURE: Proper validation of ecrecover result
         address signer = ecrecover(hash, v, r, s);
-        
+
         // CRITICAL FIX: Check that ecrecover returned a valid address
         require(signer != address(0), "Invalid signature");
-        
+
         // Check if hash has been used before
         require(!usedHashes[hash], "Hash already used");
-        
+
         // Mark hash as used
         usedHashes[hash] = true;
-        
+
         // Authorize the user
         authorizedUsers[user] = true;
-        
+
         emit UserAuthorized(user, hash);
     }
-    
+
     /**
      * @dev Alternative secure implementation using OpenZeppelin's ECDSA library
      * This is the recommended approach as it handles all edge cases automatically
      */
-    function authorizeUserWithEcdsa(
-        bytes memory signature,
-        bytes32 hash,
-        address user
-    ) external {
+    function authorizeUserWithEcdsa(bytes memory signature, bytes32 hash, address user) external {
         // This would use OpenZeppelin's ECDSA.recover() which automatically
         // reverts on invalid signatures
         // address signer = ECDSA.recover(hash, signature);
-        
+
         // For demonstration, we'll use ecrecover with proper validation
         require(signature.length == 65, "Invalid signature length");
-        
+
         bytes32 r; // 32 bytes
         bytes32 s; // 32 bytes
         uint8 v; //2 byte
-        
+
         assembly {
             r := mload(add(signature, 32))
             s := mload(add(signature, 64))
             v := byte(0, mload(add(signature, 96)))
         }
-        
+
         // Handle malleability
-        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) { // @audit SIGNATURE MALLEABILITY PROTECTION
+        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+            // @audit SIGNATURE MALLEABILITY PROTECTION
             revert("Invalid signature 's' value");
         }
-        
-        if (v != 27 && v != 28) { // v must be either 27 or 28 for legacy Ethereum signatures.
+
+        if (v != 27 && v != 28) {
+            // v must be either 27 or 28 for legacy Ethereum signatures.
             revert("Invalid signature 'v' value"); // Some off-chain signing tools may use 0/1 instead, which would need conversion, but this code assumes canonical Ethereum values.
         }
-        
+
         address signer = ecrecover(hash, v, r, s);
         require(signer != address(0), "Invalid signature"); // @audit SIGNATURE VALIDATION PROTECTION
-        
+
         // Check if hash has been used before
         require(!usedHashes[hash], "Hash already used"); // @audit REPLAY ATTACK PROTECTION
-        
+
         // Mark hash as used
         usedHashes[hash] = true;
-        
+
         // Authorize the user
         authorizedUsers[user] = true;
-        
+
         emit UserAuthorized(user, hash);
     }
-    
+
     /**
      * @dev Function that requires authorization
      */
@@ -103,7 +95,7 @@ contract SecureSignatureContract {
         require(authorizedUsers[msg.sender], "Not authorized");
         return true;
     }
-    
+
     /**
      * @dev Check if a user is authorized
      * @param user The user to check
@@ -112,7 +104,7 @@ contract SecureSignatureContract {
     function isAuthorized(address user) external view returns (bool) {
         return authorizedUsers[user];
     }
-    
+
     /**
      * @dev Get the signer from a signature (secure version)
      * @param v The v component of the signature
@@ -121,34 +113,26 @@ contract SecureSignatureContract {
      * @param hash The hash that was signed
      * @return address The recovered signer address
      */
-    function recoverSigner(
-        uint8 v, 
-        bytes32 r, 
-        bytes32 s, 
-        bytes32 hash
-    ) external pure returns (address) {
+    function recoverSigner(uint8 v, bytes32 r, bytes32 s, bytes32 hash) external pure returns (address) {
         // SECURE: Validate ecrecover result
         address signer = ecrecover(hash, v, r, s);
         require(signer != address(0), "Invalid signature");
         return signer;
     }
-    
+
     /**
      * @dev Create a hash for authorization
      * @param user The user to authorize
      * @param nonce A unique nonce
      * @return bytes32 The hash to be signed
      */
-    function createAuthorizationHash(
-        address user, 
-        uint256 nonce
-    ) external pure returns (bytes32) {
-         bytes32 hashedVal;
-         assembly {
+    function createAuthorizationHash(address user, uint256 nonce) external pure returns (bytes32) {
+        bytes32 hashedVal;
+        assembly {
             mstore(0x00, user)
             mstore(0x20, nonce)
             hashedVal := keccak256(0x00, 0x20)
         }
         return hashedVal;
     }
-} 
+}
--- src/SignatureAttacks.sol
+++ src/SignatureAttacks.sol
@@ -9,47 +9,41 @@ pragma solidity ^0.8.30;
 contract VulnerableSignatureContract {
     mapping(address => bool) public authorizedUsers;
     mapping(bytes32 => bool) public usedHashes;
-    
+
     event UserAuthorized(address indexed user, bytes32 indexed hash);
     event InvalidSignatureUsed(address indexed signer, bytes32 indexed hash);
-    
+
     /**
      * @dev Vulnerable function that recovers signer from signature without validation
      * @param v The v component of the signature
-     * @param r The r component of the signature  
+     * @param r The r component of the signature
      * @param s The s component of the signature
      * @param hash The hash that was signed
      * @param user The user to authorize
      */
-    function authorizeUser(
-        uint8 v, 
-        bytes32 r, 
-        bytes32 s, 
-        bytes32 hash,
-        address user
-    ) external {
+    function authorizeUser(uint8 v, bytes32 r, bytes32 s, bytes32 hash, address user) external {
         // VULNERABILITY: Missing validation of ecrecover result
         address signer = ecrecover(hash, v, r, s);
-        
+
         // This check is missing: require(signer != address(0), "Invalid signature");
-        
+
         // Check if hash has been used before
         require(!usedHashes[hash], "Hash already used");
-        
+
         // Mark hash as used
         usedHashes[hash] = true;
-        
+
         // Authorize the user
         authorizedUsers[user] = true;
-          
+
         emit UserAuthorized(user, hash);
-        
+
         // If signer is address(0), this indicates an invalid signature
         if (signer == address(0)) {
             emit InvalidSignatureUsed(signer, hash);
         }
     }
-    
+
     /**
      * @dev Function that requires authorization
      * @param data Some data to process
@@ -58,7 +52,7 @@ contract VulnerableSignatureContract {
         require(authorizedUsers[msg.sender], "Not authorized");
         return true;
     }
-    
+
     /**
      * @dev Check if a user is authorized
      * @param user The user to check
@@ -67,7 +61,7 @@ contract VulnerableSignatureContract {
     function isAuthorized(address user) external view returns (bool) {
         return authorizedUsers[user];
     }
-    
+
     /**
      * @dev Get the signer from a signature (vulnerable version)
      * @param v The v component of the signature
@@ -76,29 +70,21 @@ contract VulnerableSignatureContract {
      * @param hash The hash that was signed
      * @return address The recovered signer address
      */
-    function recoverSigner(
-        uint8 v, 
-        bytes32 r, 
-        bytes32 s, 
-        bytes32 hash
-    ) external pure returns (address) {
+    function recoverSigner(uint8 v, bytes32 r, bytes32 s, bytes32 hash) external pure returns (address) {
         // VULNERABILITY: No validation of ecrecover result
         address signer = ecrecover(hash, v, r, s);
         return signer; // This can return address(0) for invalid signatures
     }
-    
+
     /**
      * @dev Create a hash for authorization
      * @param user The user to authorize
      * @param nonce A unique nonce
      * @return bytes32 The hash to be signed
      */
-    function createAuthorizationHash(
-        address user, 
-        uint256 nonce
-    ) external pure returns (bytes32) {
-         bytes32 hashedVal;
-         assembly {
+    function createAuthorizationHash(address user, uint256 nonce) external pure returns (bytes32) {
+        bytes32 hashedVal;
+        assembly {
             mstore(0x00, user)
             mstore(0x20, nonce)
             hashedVal := keccak256(0x00, 0x20)
